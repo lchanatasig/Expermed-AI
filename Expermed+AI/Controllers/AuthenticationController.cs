1dde64e9f71ea8f31164e54a5d20974c749ccdd5
Fer-dev <chanatasigluis@gmail.com>
2024-10-18 11:54:23 -0500

    Agregar archivos de proyecto.

---
+++ Expermed+AI/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expermed_AI.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [ActionName("SignInBasic")]
+        public IActionResult SignInBasic()
+        {
+            return View();
+        }
+    }
+}
---
+++ Expermed+AI/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expermed_AI.Controllers
+{
+    public class UsersController : Controller
+    {
+        [ActionName("UserList")]
+        public IActionResult UserList()
+        {
+            return View();
+        }
+    }
+}
