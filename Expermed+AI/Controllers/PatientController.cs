4ccc1ecb97b6021b4a083632b347497b1269703b
Fer-dev <chanatasigluis@gmail.com>
2024-10-23 17:22:15 -0500

    new

---
+++ Expermed+AI/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expermed_AI.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult PatientList()
+        {
+            return View();
+        }
+    }
+}
